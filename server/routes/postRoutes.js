fa0e29aa2609d50cc9e8b55d469302bb5b15cc45
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-02-06 14:50:15 -0500

    updated card component and fixed share post functionality

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -10,8 +10,8 @@ const router = express.Router();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  cloud_api_key: process.env.CLOUDINARY_API_KEY,
-  cloud_api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // GET ALL POSTS
